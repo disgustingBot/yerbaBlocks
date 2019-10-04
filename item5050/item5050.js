c889cf65a052423413c9f7e36745d7460ac1bed2
tomasmparra <50798104+tomasmparra@users.noreply.github.com>
2019-10-04 18:34:09 +0200

    Update item5050.js

--- item5050/item5050.js
+++ item5050/item5050.js
@@ -11,16 +11,33 @@ registerBlockType( 'gutentag/item5050', {
 	icon: 'heart',
 	category: 'common',
 	attributes: {
+		itemTitleH5: { type: 'array', source: 'children', selector: '.itemTitleH5' },
+		item5050Img: { attribute: 'src', selector: '.item5050Img' },
+		item5050Alt: { attribute: 'alt', selector: '.item5050Img' },
+
 	},
 
 	edit( { attributes, setAttributes } ) {
-		const getImgButton = openEvent => attributes.fPSectionImg ? <img src={ attributes.fPSectionImg } onClick={ openEvent } className={ 'fPSectionImg itemImg' } alt={ 'si' } /> : <div style={ styleButton } className={ 'itemImg' }><Button onClick={ openEvent } className="button button-large" >Pick an image</Button></div>;
+		const getImgButton = openEvent => attributes.item5050Img ? <img src={ attributes.item5050Img } onClick={ openEvent } className={ 'item5050Img itemImg' } alt={ 'si' } /> : <div className={ 'itemImg' }><Button onClick={ openEvent } className="button button-large" >Pick an image</Button></div>;
 
 		return (
-      <figure class="item5050 SectionItem">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/home/view-sandy-beach_23-2147836788.jpg" alt="" class="item5050Img itemImg" />
-        <figcaption class="localizacionItemFigcaption itemTitleCenter itemTitle">
-          <a href="#"><h5>Ibiza</h5></a>
+      <figure className="item5050 SectionItem">
+				<MediaUpload
+					onSelect={ media => {
+						setAttributes( { item5050Img: media.url, item5050Alt: media.alt } );
+					} }
+					type="image"
+					value={ attributes.item5050Img }
+					render={ ( { open } ) => getImgButton( open ) }
+				/>
+        <figcaption className="localizacionItemFigcaption itemTitleCenter itemTitle">
+          <h5 className="itemTitleH5">
+						<RichText
+							onChange={ content => setAttributes( { itemTitleH5: content } ) }
+							value={ attributes.itemTitleH5 }
+							placeholder="contenido"
+						/>
+					</h5>
         </figcaption>
       </figure>
 		);
@@ -30,10 +47,10 @@ registerBlockType( 'gutentag/item5050', {
 		const image = ( src, alt, className ) => src ? <img src={ src } className={ className } alt={ alt } /> : null;
 
 		return (
-      <figure class="item5050 SectionItem">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/home/view-sandy-beach_23-2147836788.jpg" alt="" class="item5050Img itemImg" />
-        <figcaption class="localizacionItemFigcaption itemTitleCenter itemTitle">
-          <a href="#"><h5>Ibiza</h5></a>
+      <figure className="item5050 SectionItem">
+				{ image( attributes.item5050Img, attributes.item5050Alt, 'item5050Img itemImg' ) }
+        <figcaption className="localizacionItemFigcaption itemTitleCenter itemTitle">
+				<h5 className="itemTitleH5">{ attributes.itemTitleH5 }</h5>
         </figcaption>
       </figure>
 		);
